005fcd5aadd17cab21eb2639ad0c7bdaf677b175
Haoran Li <lihr04@outlook.com>
2021-03-16 16:55:29 -0400

    Add bash

---
+++ bash/SCP.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=SCP
+#SBATCH --time=5:0:0
+#SBATCH --partition=gpuk80
+#SBATCH --gres=gpu:1
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mail-type=end
+#SBATCH --mail-user=hli143@jhu.edu
+
+ml anaconda
+ml cuda/10.1
+conda activate torch
+mkdir -p logs
+python grid_search.py -r SCP --id $SLURM_ARRAY_TASK_ID -p 4 5 6 7 8 -s 50 > logs/SCP_$SLURM_ARRAY_TASK_ID
+echo "Finished with job $SLURM_JOBID task $SLURM_ARRAY_TASK_ID"
---
+++ bash/SketchMAS_bucket.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=SketchMAS_bucket
+#SBATCH --time=5:0:0
+#SBATCH --partition=gpuk80
+#SBATCH --gres=gpu:1
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mail-type=end
+#SBATCH --mail-user=hli143@jhu.edu
+
+ml anaconda
+ml cuda/10.1
+conda activate torch
+mkdir -p logs
+python grid_search.py -r SketchMAS --id $SLURM_ARRAY_TASK_ID -p 5 -b 10 20 30 40 50 --result-filename "bucket_10-50" > logs/SketchMAS_bucket_$SLURM_ARRAY_TASK_ID
+echo "Finished with job $SLURM_JOBID task $SLURM_ARRAY_TASK_ID"
+
