ff48b8b5da66d41dec0a51d7ce721b9f712cc33b
Haoran Li <lihr04@outlook.com>
2021-03-22 16:15:54 -0400

    result on adding up W

---
+++ bash/SketchMAS_add.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=SketchMAS
+#SBATCH --time=5:0:0
+#SBATCH --partition=gpuk80
+#SBATCH --gres=gpu:1
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mail-type=end
+#SBATCH --mail-user=hli143@jhu.edu
+
+ml anaconda
+ml cuda/10.1
+conda activate torch
+mkdir -p logs
+python grid_search.py -r SketchMAS --id $SLURM_ARRAY_TASK_ID -p 2 3 4 5 6 -b 50 --result-filename "add" > logs/SketchMAS_$SLURM_ARRAY_TASK_ID
+echo "Finished with job $SLURM_JOBID task $SLURM_ARRAY_TASK_ID"
+
--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
-sbatch -o logs/slurm-%A_%a.out --array=0-4 bash/SketchEWC_10bucket.sh
-sbatch -o logs/slurm-%A_%a.out --array=0-4 bash/SketchEWC_bucket.sh
-sbatch -o logs/slurm-%A_%a.out --array=0-4 bash/SketchMAS_bucket.sh
+sbatch -o logs/slurm-%A_%a.out --array=0 bash/SketchEWC_add.sh
+sbatch -o logs/slurm-%A_%a.out --array=0 bash/SketchMAS_add.sh
 
